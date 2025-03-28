2cf3f25258f0483c22eeaa0eaa345c97453a54df
Ehmad Saeed <justehmadsaeed@gmail.com>
2025-03-28 09:18:05 +0500

    Refactor traffic data producer to use a function for event generation and improve error handling

--- producer.py
+++ producer.py
@@ -1,22 +1,36 @@
-import time
 import random
 import json
 from kafka import KafkaProducer
+from datetime import datetime
+import time
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 TOPIC = "traffic_data"
+CONGESTION_LEVELS = ["LOW", "MEDIUM", "HIGH"]
+SENSORS = ['S101', 'S202', 'S303', 'S404', 'S505']
 SECONDS_TO_WAIT = 1
-congestion_levels = ["LOW", "MEDIUM", "HIGH"]
-while True:
-    data = {
-        "sensor_id": random.randint(1, 5),
-        "timestamp": time.time(),
+
+
+def generate_traffic_event(sensor_id):
+    return {
+        "sensor_id": sensor_id,
+        "timestamp": datetime.now().isoformat(),
         "vehicle_count": random.randint(1, 100),
         "average_speed": round(random.uniform(10, 130), 1),
-        "congestion_level": random.choice(congestion_levels),
+        "congestion_level": random.choice(CONGESTION_LEVELS),
     }
-    producer.send(TOPIC, value=data)
-    print(f"Sent: {data}")
+
+
+while True:
+    sensor_id = random.choice(SENSORS)
+    data = generate_traffic_event(sensor_id)
+    try:
+        producer.send(TOPIC, value=data)
+        print(f"Sent: {data}")
+    except Exception as e:
+        print(f"Failed to send: {data}")
+        print(e)
+
     time.sleep(SECONDS_TO_WAIT)
